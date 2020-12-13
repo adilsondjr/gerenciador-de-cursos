8d948eb802c10f184cae005679d23f488e2f5a2a
Adílson Domiciano Júnior <adilsondjr@inatel.br>
2020-12-13 11:06:23 -0300

    criando um tratamento personalizado para o @valid

--- src/main/java/com/adilson/escola/cursos/gradecurricular/handler/ResourceHandler.java
+++ src/main/java/com/adilson/escola/cursos/gradecurricular/handler/ResourceHandler.java
@@ -1,14 +1,38 @@
 package com.adilson.escola.cursos.gradecurricular.handler;
 
 import com.adilson.escola.cursos.gradecurricular.exception.MateriaException;
+import com.adilson.escola.cursos.gradecurricular.model.ErrorMapResponse;
+import com.adilson.escola.cursos.gradecurricular.model.ErrorMapResponse.ErrorMapResponseBuilder;
 import com.adilson.escola.cursos.gradecurricular.model.ErrorResponse;
 import com.adilson.escola.cursos.gradecurricular.model.ErrorResponse.ErrorResponseBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class ResourceHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMapResponse> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String field = ((FieldError) error ).getField();
+            String message = error.getDefaultMessage();
+            errors.put(field, message);
+        });
+
+        ErrorMapResponseBuilder errorMap = ErrorMapResponse.builder();
+        errorMap.errors(errors).httpStatus(HttpStatus.BAD_REQUEST.value()).timeStamp(System.currentTimeMillis());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMap.build());
+    }
 
     @ExceptionHandler(MateriaException.class)
     public ResponseEntity<ErrorResponse> handlerMateriaException(MateriaException ex) {
